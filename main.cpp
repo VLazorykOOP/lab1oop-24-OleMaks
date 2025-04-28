e28579c6d3812e9879743e6dedf1428a2cc3574b
OleMaks <olenych.maksym@chnu.edu.ua>
2025-04-29 00:13:20 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int N = 7; // розмір
+    int A[N] = {0, 3, 0, 5, 7, 0, 9}; // приклад
+
+    int count_zeros = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] == 0) {
+            count_zeros++;
+        }
+    }
+
+    cout << "Number of 0 elements: " << count_zeros << endl;
+
     return 0;
 }
